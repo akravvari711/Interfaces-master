069219cad9c927e1ce0fa22100708c89c882e2b7
Amalia Kravvari <amalia.kravvari@Peoplecert.onmicrosoft.com>
2020-11-05 16:35:20 +0200

    Add project files.

---
+++ Interfaces/Models/Boat.cs
@@ -0,0 +1,43 @@
+﻿using Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Models
+{
+    class Boat : AutoMobile, IAutoMobileActions, IBoatActions
+    {
+
+        public Boat()
+        {
+
+        }
+
+        public Boat(bool IsOn)
+        {
+            this.IsOn = IsOn;
+        }
+
+        public void SteerLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reverse()
+        {
+            Console.WriteLine("The boat is reversing");
+        }
+
+        public void SteerRight()
+        {
+            Console.WriteLine("The boat is steering right");
+        }
+
+        public void Steer(bool steerLeft)
+        {
+            Console.WriteLine("The boat is steering");
+        }
+    }
+}
